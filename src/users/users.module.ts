59919279fc4aa28741ecf9c8ba735daa0cb1c59e
Massamba-commits <paparafinha20@gmail.com>
2026-06-24 21:08:07 +0000

    feat(auth): JWT strategy, guard et RBAC roles decorator

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, AuthService, JwtStrategy],
   controllers: [UsersController],
   exports: [UsersService],
 })
