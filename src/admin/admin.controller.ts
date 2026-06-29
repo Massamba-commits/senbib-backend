0fe46029605505ca027a24d1dfdae48916718eb6
Massamba-commits <paparafinha20@gmail.com>
2026-06-29 19:07:21 +0000

    feat(admin): finalisation module admin

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Delete, Param, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jtw-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
 import { Role } from '../users/user.entity';
